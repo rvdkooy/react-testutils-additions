223b8fdeba026040790368b551b3fe4339367207
Ronald van der Kooij <ronald@sharpsolutions.nl>
2017-04-10 14:46:05 +0200

    removed karma gulp task, don't need it anymore

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var basePath = './';
 var jshint = require('gulp-jshint');
 var gulp = require('gulp');
-var karma = require('gulp-karma');
 var fs =require('fs');
 
 gulp.task('jshint', function () {
@@ -15,16 +14,6 @@ gulp.task('watches', function(){
         basePath + '*.js', 
         basePath + '__tests__/*.js'
     ], ['jshint', 'tests']);
-});
-
-gulp.task('tests', function () {
-    return gulp.src(['./fake/*.js'])
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'run'
-        })).on('error', function (err) {
-            console.log(err)
-        });
 });
 
 gulp.task('default', [ 'tests', 'jshint', 'watches' ]);
